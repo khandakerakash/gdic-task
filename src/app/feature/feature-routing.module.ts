db9cf778330921fe620cf760eddbc43e349d42a0
khandakar akash <akash.cse10@gmail.com>
2022-05-26 13:11:33 +0600

    Dashboard module create and Feature Routing setup are done.

--- src/app/feature/feature-routing.module.ts
+++ src/app/feature/feature-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../core/guard/auth.guard';
+import { PublicComponent } from '../core/layout-components/view/public';
+import { SecureComponent } from '../core/layout-components/view/secure';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: PublicComponent,
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+    data: { title: 'Auth' }
+  },
+  {
+    path: 'dashboard',
+    component: SecureComponent,
+    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),
+    data: { title: 'Dashboard' },
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'employee',
+    component: SecureComponent,
+    loadChildren: () => import('./employee/employee.module').then(m => m.EmployeeModule),
+    data: { title: 'Employee' },
+    canActivate: [AuthGuard]
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
