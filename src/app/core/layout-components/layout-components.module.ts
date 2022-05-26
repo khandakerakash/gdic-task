2b0ae57ea49723047d56a097caf07501ed488daa
khandakar akash <akash.cse10@gmail.com>
2022-05-26 13:06:17 +0600

    Layout Components Module is modified.

--- src/app/core/layout-components/layout-components.module.ts
+++ src/app/core/layout-components/layout-components.module.ts
@@ -5,19 +5,22 @@ import { SecureComponent } from './view/secure/secure.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
 
-
+const components = [
+  PublicComponent,
+  SecureComponent,
+  HeaderComponent,
+  SidebarComponent,
+  FooterComponent
+];
 
 @NgModule({
-  declarations: [
-    PublicComponent,
-    SecureComponent,
-    HeaderComponent,
-    FooterComponent,
-    SidebarComponent
-  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule
+  ],
+  declarations: components,
+  exports: components
 })
 export class LayoutComponentsModule { }
